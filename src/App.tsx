4eaac9b70cae72b87c876630475a1d666b183cf8
Fanumfat <christophergomez424@gmail.com>
2025-02-25 10:48:44 -0500

    student profiles

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,24 @@
-import { useEffect, useState } from "react";
-import {useAuthenticator} from '@aws-amplify/ui-react';
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-
-const client = generateClient<Schema>();
- 
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useAuthenticator } from "@aws-amplify/ui-react";
+import StudentProfile from "./components/StudentProfile.tsx";
+import "./App.css";
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const {user, signOut} = useAuthenticator();
- 
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
-  
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
+  const { } = useAuthenticator();
 
   return (
     <main>
-      <h1>{user?.signInDetails?.loginId} 's todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}
-            <button onClick={() => deleteTodo(todo.id)}>x</button>
-            <button onClick={signOut}>Logout</button>
-          </li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
+      
+
+
+      <BrowserRouter>
+        <nav>
+          <Link to="/profile">Profile</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/profile" element={<StudentProfile />} />
+        </Routes>
+      </BrowserRouter>
     </main>
   );
 }
